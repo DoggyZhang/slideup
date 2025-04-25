72872b8d187307e1665d7acf6aac4c03c0e68010
DoggyZhang <afei572924155@live.com>
2025-04-25 17:43:16 +0800

    Slide平滑移动框架

---
+++ motionlayout-start/app/src/main/java/com/google/samples/motionlayoutcodelab/SlideUpDismissActivity.kt
@@ -0,0 +1,61 @@
+/*
+ *   Copyright (C) 2019 The Android Open Source Project
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.google.samples.motionlayoutcodelab
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.samples.slideup.SlideUp
+import com.google.samples.slideup.SlideUpBuilder
+
+class SlideUpDismissActivity : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_slide_up_dismiss)
+
+        val slideView = findViewById<View>(R.id.v_slide)
+
+        slideView.setOnClickListener {
+            Toast.makeText(this@SlideUpDismissActivity, "Click SlideView", Toast.LENGTH_SHORT).show()
+        }
+
+
+        val subView = findViewById<View>(R.id.v_sub)
+        subView.setOnClickListener {
+            Toast.makeText(this@SlideUpDismissActivity, "Click SubView", Toast.LENGTH_SHORT).show()
+        }
+
+        val slideUp = SlideUpBuilder(slideView)
+            .withStartState(SlideUp.State.SHOWED)
+            .withSlideDirection(SlideUp.SlideDirection.UP)
+            .withListeners(object : SlideUp.Listener.Events {
+                override fun onVisibilityChanged(visibility: Int) {
+                    Log.d("zhangfei", "onVisibilityChanged, visibility:$visibility")
+                }
+
+                override fun onSlide(percent: Float) {
+                    Log.d("zhangfei", "onSlide, percent:$percent")
+                }
+            })
+            .build()
+    }
+
+}
---
+++ motionlayout-start/app/src/main/java/com/google/samples/slideup/VerticalTouchConsumer.java
@@ -0,0 +1,111 @@
+package com.google.samples.slideup;
+
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * @author pa.gulko zTrap (05.07.2017)
+ */
+class VerticalTouchConsumer extends TouchConsumer {
+    private boolean mGoingUp = false;
+    private boolean mGoingDown = false;
+
+    VerticalTouchConsumer(SlideUpBuilder builder, LoggerNotifier notifier, AnimationProcessor animationProcessor) {
+        super(builder, notifier, animationProcessor);
+    }
+
+    boolean slideDown(View touchedView, MotionEvent event) {
+        Log.d("zhangfei", "consumeBottomToTop, touchedView: " + touchedView + ", event:" + event.getActionMasked());
+        float touchedArea = event.getY();
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                mViewHeight = mBuilder.mSliderView.getHeight();
+                mStartPositionY = event.getRawY();
+                mViewStartPositionY = mBuilder.mSliderView.getTranslationY();
+//                mCanSlide = touchFromAlsoSlide(touchedView, event);
+//                mCanSlide |= mBuilder.mTouchableArea >= touchedArea;
+                Log.d("zhangfei", "slideDown(ACTION_DOWN) -> mViewHeight: " + mViewHeight + ", mCanSlide:" + mCanSlide);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float difference = event.getRawY() - mStartPositionY;
+                float moveTo = mViewStartPositionY + difference;
+                float percents = moveTo * 100 / mBuilder.mSliderView.getHeight();
+                calculateDirection(event);
+
+                Log.d("zhangfei", "slideDown(ACTION_MOVE) -> difference: " + difference + ", moveTo:" + moveTo + ", mCanSlide:" + mCanSlide);
+                if (mBuilder.mSlideDirection == SlideUp.SlideDirection.DOWN && moveTo > 0 && mCanSlide) {
+                    mNotifier.notifyPercentChanged(percents);
+                    mBuilder.mSliderView.setTranslationY(moveTo);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                float slideAnimationFrom = mBuilder.mSliderView.getTranslationY();
+                if (slideAnimationFrom == mViewStartPositionY) {
+                    return !Internal.isUpEventInView(mBuilder.mSliderView, event);
+                }
+                boolean scrollableAreaConsumed = mBuilder.mSliderView.getTranslationY() > mBuilder.mSliderView.getHeight() / 5;
+
+                if (scrollableAreaConsumed && mGoingDown) {
+                    mAnimationProcessor.setValuesAndStart(slideAnimationFrom, mBuilder.mSliderView.getHeight());
+                } else {
+                    mAnimationProcessor.setValuesAndStart(slideAnimationFrom, 0);
+                }
+                mCanSlide = true;
+                mGoingUp = false;
+                mGoingDown = false;
+                break;
+        }
+        mPrevPositionY = event.getRawY();
+        mPrevPositionX = event.getRawX();
+        return true;
+    }
+
+    boolean slideUp(View touchedView, MotionEvent event) {
+        float touchedArea = event.getY();
+        switch (event.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                mViewHeight = mBuilder.mSliderView.getHeight();
+                mStartPositionY = event.getRawY();
+                mViewStartPositionY = mBuilder.mSliderView.getTranslationY();
+//                mCanSlide = touchFromAlsoSlide(touchedView, event);
+//                mCanSlide |= getBottom() - mBuilder.mTouchableArea <= touchedArea;
+                Log.d("zhangfei", "slideUp(ACTION_DOWN) -> mViewHeight: " + mViewHeight + ", mCanSlide:" + mCanSlide);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float difference = event.getRawY() - mStartPositionY;
+                float moveTo = mViewStartPositionY + difference;
+                float percents = moveTo * 100 / -mBuilder.mSliderView.getHeight();
+                calculateDirection(event);
+
+                Log.d("zhangfei", "slideUp(ACTION_MOVE) -> difference: " + difference + ", moveTo:" + moveTo + ", mCanSlide:" + mCanSlide);
+                if (mBuilder.mSlideDirection == SlideUp.SlideDirection.UP && moveTo < 0 && mCanSlide) {
+                    mNotifier.notifyPercentChanged(percents);
+                    mBuilder.mSliderView.setTranslationY(moveTo);
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                float slideAnimationFrom = -mBuilder.mSliderView.getTranslationY();
+                if (slideAnimationFrom == mViewStartPositionY) {
+                    return !Internal.isUpEventInView(mBuilder.mSliderView, event);
+                }
+                boolean scrollableAreaConsumed = mBuilder.mSliderView.getTranslationY() < -mBuilder.mSliderView.getHeight() / 5;
+
+                if (scrollableAreaConsumed && mGoingUp) {
+                    mAnimationProcessor.setValuesAndStart(slideAnimationFrom, mBuilder.mSliderView.getHeight() + mBuilder.mSliderView.getTop());
+                } else {
+                    mAnimationProcessor.setValuesAndStart(slideAnimationFrom, 0);
+                }
+                mCanSlide = true;
+                break;
+        }
+        mPrevPositionY = event.getRawY();
+        mPrevPositionX = event.getRawX();
+        return true;
+    }
+
+    private void calculateDirection(MotionEvent event) {
+        mGoingUp = mPrevPositionY - event.getRawY() > 0;
+        mGoingDown = mPrevPositionY - event.getRawY() < 0;
+    }
+}
